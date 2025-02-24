96fd28745f08272656a0315eb40048e56ad65c41
SaraBarrett <sara.c.mon@gmail.com>
2025-02-24 14:02:02 +0000

    JS intro

---
+++ js/dom.js
@@ -0,0 +1,4 @@
+let myHistoryTag = document.querySelectorAll(".mw-headline");
+
+console.log(myHistoryTag);
+myHistoryTag.innerText = "História";
---
+++ js/index.js
@@ -0,0 +1,35 @@
+// let shoppingList = [];
+// let item = "";
+// // let myFirstName = prompt("Qual é o teu primeiro nome?");
+// // let mySurname = prompt("Qual é o teu apelido?");
+
+// // alert('olá ' +myFirstName + " " + mySurname);
+// while (item != "fim") {
+//   item = prompt(
+//     "Adiciona um item. Se digitares fim ele conclui, se te enganares coloca apagar"
+//   ).toLowerCase();
+
+//   if (item == "apagar" && shoppingList.length > 0) {
+//     shoppingList.pop();
+//   } else {
+//     shoppingList.push(item);
+//   }
+// }
+
+// shoppingList.pop();
+// let element;
+
+// for (element of shoppingList) {
+//   alert(element);
+// }
+
+// let user = {
+//     'name': 'Sara',
+//     'age': 38,
+//     'isTeacher': true,
+// }
+
+// const fullNames = [   { first: 'Albus', last: 'Dumbledore' },   { first: 'Harry', last: 'Potter' },   { first: 'Hermione', last: 'Granger' },   { first: 'Ron', last: 'Weasley' },   { first: 'Rubeus', last: 'Hagrid' },   { first: 'Minerva', last: 'McGonagall' },   { first: 'Severus', last: 'Snape' } ];
+
+// const names = fullNames.map(name=>name.first)
+console.dir(document);
